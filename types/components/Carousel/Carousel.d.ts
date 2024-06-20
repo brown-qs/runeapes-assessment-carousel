400d059b37dbe3dc7fbf222a6a4cd2d45b6e133c
brown-qs <neoprince1992@gmail.com>
2024-06-21 07:02:08 +0900

    carousel complete

--- types/components/Carousel/Carousel.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export interface CarouselProps {
-    label: string;
-}
-declare const Carousel: ({ label }: CarouselProps) => React.JSX.Element;
-export default Carousel;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,8 @@
-export * from './components';
+import React from "react";
+
+export interface CarouselProps {
+  itemWidth: number;
+  isInfinite: boolean;
+}
+declare const Carousel: ({ itemWidth, isInfinite }: CarouselProps) => React.JSX.Element;
+export default Carousel;
